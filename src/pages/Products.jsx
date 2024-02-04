757f706e290d9d151afc706e89d41f0dbead4d9a
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-04 17:06:42 +0200

    searchParams

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,32 @@
-import { Link } from 'react-router-dom';
+import { useSearchParams, Link } from 'react-router-dom';
 
 const Products = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const productId = searchParams.get('productId');
+
   return (
     <div>
-      {['apple ', 'lemon ', 'kiwi ', 'orange ', 'pear '].map(product => {
-        return (
-          <Link key={product} to={`${product}`}>
-            {product}
-          </Link>
-        );
-      })}
+      <input
+        type="text"
+        value={productId}
+        onChange={evt => setSearchParams({ productId: evt.target.value })}
+      />
+      <button onClick={() => setSearchParams({ c: 'hello' })}>Change sp</button>
+      <ul>
+        {[
+          'product-1 ',
+          'product-2 ',
+          'product-3 ',
+          'product-4 ',
+          'product-5 ',
+        ].map(product => {
+          return (
+            <li key={product}>
+              <Link to={`${product}`}>{product}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
