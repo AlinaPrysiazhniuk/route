3cb1907f3391c2c4ef70e45161dce70f0811ef36
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2024-02-04 17:43:00 +0200

    queryString

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,25 +1,35 @@
+import { useState } from 'react';
 import { useSearchParams, Link } from 'react-router-dom';
 
 const Products = () => {
+  const [products, setProducts] = useState([
+    'product-1 ',
+    'product-2 ',
+    'product-3 ',
+    'product-4 ',
+    'product-5 ',
+  ]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const productId = searchParams.get('productId');
+  const productId = searchParams.get('productId') ?? '';
+
+  const updateQueryString = evt => {
+    const productIdValue = evt.target.value;
+    if (productIdValue === '') {
+      return setSearchParams({});
+    }
+    setSearchParams({ productId: productIdValue });
+  };
+
+  const visibleProducts = products.filter(product =>
+    product.includes(productId)
+  );
 
   return (
     <div>
-      <input
-        type="text"
-        value={productId}
-        onChange={evt => setSearchParams({ productId: evt.target.value })}
-      />
-      <button onClick={() => setSearchParams({ c: 'hello' })}>Change sp</button>
+      <input type="text" value={productId} onChange={updateQueryString} />
+
       <ul>
-        {[
-          'product-1 ',
-          'product-2 ',
-          'product-3 ',
-          'product-4 ',
-          'product-5 ',
-        ].map(product => {
+        {visibleProducts.map(product => {
           return (
             <li key={product}>
               <Link to={`${product}`}>{product}</Link>
